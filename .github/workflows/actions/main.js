77017f071e5d39942aa8f732d7e959ca0aa723ac
Soumya Ghosh <soumya@soumya-HP-Notebook>
2024-09-24 17:09:31 +0530

    initial commit

---
+++ .github/workflows/actions/main.js
@@ -0,0 +1,17 @@
+const core = require('@actions/core');
+const { exec } = require('@actions/exec');
+
+function run() {
+    const bucket = core.getInput('bucket', { required: true });
+    const bucketRegion = core.getInput('bucket-region', { required: true });
+    const distFolder = core.getInput('dist-folder', { required: true });
+
+    const s3Uri = `s3://${bucket}`;
+
+    exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+
+    const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
+    core.setOutput('website-url', websiteUrl);
+}
+
+run();
